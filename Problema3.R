85e612a56db59ab70e9a6b492d9c20e72947bcb0
YeraiSarmiento <yerai.sarmiento@estudiantat.upc.edu>
2025-02-18 09:58:06 +0100

    Update Problema3.R
    ...

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,10 @@
 #Codigo para problema 3
+  #el apartado 1 es fácil (con un sum(x))
+
+#Para extraer numeros
+x <- 10:20
+x
+x[-c(3,5,7)] #esto extrae los elementos en las posiciones 3,5 y 7
+
+#apartado 2
+sum(exp(x))-sum(exp(y)) #con sus respectivos valores
